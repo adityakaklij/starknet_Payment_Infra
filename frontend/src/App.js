b2a0f2a575a24284e3d45e10c2118c119f1a4c0b
Aditya K <adityakaklij11@gmail.com>
2023-01-29 11:08:19 +0530

    react pahe

---
+++ frontend/src/App.js
@@ -0,0 +1,41 @@
+
+import './App.css';
+import { BrowserRouter as Router, Switch, Route, HashRouter,} from "react-router-dom";
+import Navbar from './Components/Navbar';
+import Home from './Components/Home';
+import Deploy from './Components/Deploy';
+import Analytics from './Components/Analytics';
+
+
+function App() {
+  return (
+    <>
+      <HashRouter basename='/'>
+    <div className="App">
+       <Navbar/>
+
+      <Switch>
+
+          <Route exact path="/">
+            <Home/>
+          </Route>
+
+          <Route exact path="/Deploy">
+            <Deploy/>
+          </Route>
+
+          <Route exact path="/Analytics">
+            <Analytics/>
+          </Route>
+
+
+          
+        </Switch>
+
+    </div>
+    </HashRouter>
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/Components/Deploy.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Deploy() {
+  return (
+    <div>
+        <h1>Deploy</h1>
+    </div>
+  )
+}
+
+export default Deploy
